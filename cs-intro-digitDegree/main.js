0538932911b69e291ef75de410e3a63b68c2a696
Alvin Cho <alvinxcho2@gmail.com>
2022-12-09 00:25:45 +0000

    adjusted while loop for digitDegree

---
+++ cs-intro-digitDegree/main.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+// function solution(n) {
+//   const numberString = '';
+//   let counter = 0;
+//   while (n > 9) {
+//     counter++;
+
+//   }
+//   return counter;
+// }
